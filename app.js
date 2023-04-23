5bd9315560c413e00d1fa2c6ff49fa9fd45f14ac
TEJA <39546736+saiteja1290@users.noreply.github.com>
2023-04-23 22:26:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// Get the button
+var contactButton = document.getElementsByClassName("btn");
+
+// Add an event listener to the button
+contactButton.addEventListener("click", function() {
+  window.location.href = "https://mail.google.com/mail/?view=cm&fs=1&to=saiteja.palegar@gmail.com";
+});
