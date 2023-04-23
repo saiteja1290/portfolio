92c6ba9b341835f03bb85aabe0cc5d912bb577b4
TEJA <39546736+saiteja1290@users.noreply.github.com>
2023-04-23 22:24:06 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// Get the button
-var contactButton = document.getElementById("contact-button");
-
-// Add an event listener to the button
-contactButton.addEventListener("click", function() {
-  window.location.href = "https://mail.google.com/mail/?view=cm&fs=1&to=saiteja.palegar@gmail.com";
-});
-
